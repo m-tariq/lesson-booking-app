9296f9093a5da2cd688832a16cc880c98e8b7aff
Tariq Muhammad <tariqmuhammad.appelit@nextbridge.com>
2024-11-19 01:34:26 +0500

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/master/",
+  publicPath: "/lesson-booking-app/", // Using your repository name
 });
